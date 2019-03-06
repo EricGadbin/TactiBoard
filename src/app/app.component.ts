7ee0cc5fe21c78cb46913a23708e6e076598da64
EricGadbin <eric.gadbin@epitech.eu>
2019-03-06 16:08:30 +0100

    angular added

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TactiBoard, the best board of boards';
+}
